441fcfcc0a8efb9426d3bd53933ece6eacc43cd7
timur <timurkakhkharov@gmail.com>
2021-08-18 22:54:19 +0500

    clock

---
+++ script.js
@@ -0,0 +1,83 @@
+
+var num = +prompt('Введите день недели');
+
+switch (num) {
+    case 0:
+        alert('Ночь');
+        break;
+    case 1:
+        alert('Ночь');
+        break;
+    case 2:
+        alert('Ночь');
+        break;
+    case 3:
+        alert('Ночь');
+        break;
+    case 4:
+        alert('Ночь');
+        break;
+    case 5:
+        alert('Ночь');
+        break;
+    case 6:
+        alert('Ночь');
+        break;
+    case 7:
+        alert('Утро');
+        break;
+    case 8:
+        alert('Утро');
+        break;
+    case 9:
+        alert('Утро');
+        break;
+    case 10:
+        alert('Утро');
+        break;
+    case 11:
+        alert('Утро');
+        break;
+    case 12:
+        alert('Утро');
+        break;
+    case 13:
+        alert('Полдень');
+        break;
+    case 14:
+        alert('Полдень');
+        break;
+    case 15:
+        alert('Полдень');
+        break;
+    case 16:
+        alert('Полдень');
+        break;
+    case 17:
+        alert('Полдень');
+        break;
+    case 18:
+        alert('Вечер');
+        break;
+    case 19:
+        alert('Вечер');
+        break;
+    case 20:
+        alert('Вечер');
+        break;
+    case 21:
+        alert('Вечер');
+        break;
+    case 22:
+        alert('Вечер');
+        break;
+    case 23:
+        alert('Вечер');
+        break;
+    case 24:
+        alert('Вечер');
+        break;
+    default:
+        alert('Такого периода не существует');
+        break;
+} 
